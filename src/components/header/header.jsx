45ba0cbbf55562b036f13488e2ddb011f0e55cad
cnyet <yateup@gmail.com>
2018-09-17 15:51:14 +0800

    update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,8 +5,10 @@ export default class Header extends Component {
   constructor(props){
     super(props);
     this.state = {
-      date: new Date()
-    }
+      date: new Date(),
+      isToggleOn: true
+    };
+    this.handleClick = this.handleClick.bind(this)
   }
   //组件挂载
   componentDidMount(){
@@ -18,6 +20,15 @@ export default class Header extends Component {
   componentWillUnmount(){
     clearInterval(this.timerId);
   }
+  handleClick(){
+    this.setState(prevState =>({
+      isToggleOn: !prevState.isToggleOn
+    }))
+  }
+  preventPop(name, e){
+    e.preventDefault();
+    console.log(name);
+  }
   tick(){
     this.setState({
       date: new Date()
@@ -25,9 +36,13 @@ export default class Header extends Component {
   }
   render(){
     return (
-      <div>
-        <h1>hello world {this.props.name}</h1>
-        <h2>It is { this.state.date.toLocaleTimeString() }</h2>
+      <div className="contianer">
+        <header></header>
+        <div className="content">
+          <h1>hello world {this.props.name}</h1>
+          <h2>It is { this.state.date.toLocaleTimeString() }</h2>
+          <button onClick={(e)=>this.preventPop("tom", e)}>点击</button>
+        </div>
       </div>
     )
   }
