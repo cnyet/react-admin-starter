5ae145bd97c74bf0785821ec0243805b12edd26a
cnyet <yateup@gmail.com>
2019-05-08 23:22:52 +0800

    update version

--- src/components/home/todoItem.jsx
+++ src/components/home/todoItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
 
 class TodoItem extends Component {
   constructor(props) {
