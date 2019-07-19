b7914871fc34322d131f72fb37f968ae2d4ac84b
cnyet <yateup@gmail.com>
2019-07-19 22:19:32 +0800

    udpaye index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// progressive web application (PWA) 渐进式web应用
+// 以web技术开发手机应用，当访问https的服务器时，首次访问会将网页存储在本地浏览器，下次访问更快或断网时也可显示网页
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
