e9f4b035e8b90a1bb727d6898e427e4d0d2c5d07
cnyet <yateup@gmail.com>
2019-08-07 22:54:12 +0800

    update todo.js

--- src/pages/todo/index.js
+++ src/pages/todo/index.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import { TodoWrapper, TodoList } from './style';
 
 class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: 'hello',
+      list: []
+    }
+  }
+  changeHandle(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  clickHandle() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue]
+    })
+  }
+  deleteHandle(index) {
+    console.log(index);
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({
+      list: list
+    })
+  }
   render() {
     return (
       <TodoWrapper>
-        <input className="todo-input" type="text" />
-        <button className="todo-btn">Add</button>
+        <input className="todo-input" value={this.state.inputValue} onChange={this.changeHandle.bind(this)} />
+        <button className="todo-btn" onClick={this.clickHandle.bind(this)}>Add</button>
         <TodoList>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
+          {
+            this.state.list.map((item, index) => {
+              return <li key={index} onClick={this.deleteHandle.bind(this, index)}>{item}</li>;
+            })
+          }
         </TodoList>
       </TodoWrapper>
     )
