c0b2054a33004e3d57258a7bdc2fcdd7de91dbe9
cnyet <yateup@gmail.com>
2019-11-15 13:05:35 +0800

    update detail

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { actionCreaters } from './store';
-import { DetailWrapper, Header, Author, Content } from './style';
+import { DetailWrapper, Header, Author, Content, Reference } from './style';
+import referL from '../../statics/images/bg-l.png';
+import referR from '../../statics/images/bg-r.png';
 
 class Detail extends PureComponent {
   componentDidMount() {
@@ -21,10 +23,19 @@ class Detail extends PureComponent {
               <div className="author-desc">{id} - 阅读：{detailData.get('page_view')}</div>
             </div>
           </Author>
+          <Reference>
+            <img src={referL} alt="" class="bg1 bg" />
+            <img src={referR} alt="" class="bg2 bg" />
+            <div className="famous-word">
+              古之立大事者，不唯有超世之才，亦必有坚韧不拔之志。
+              <p className="author">——苏轼</p>
+            </div>
+          </Reference>
           <Content>
             <img src={detailData.get('article_img_01')} alt="article_img"/>
             <p>{detailData.get('article_paragraph_01')}</p>
             <p>{detailData.get('article_paragraph_02')}</p>
+            
           </Content>
         </DetailWrapper>
       );
--- src/pages/detail/style.js
+++ src/pages/detail/style.js
@@ -46,3 +46,35 @@ export const Content = styled.div`
     margin: 15px 0;
   }
 `;
+
+export const Reference = styled.div`
+  width: 100%;
+  padding: 26px 72px;
+  box-sizing: border-box;
+  background: #f8fafc;
+  border-radius: 8px;
+  margin: 32px 0 28px;
+  position: relative;
+  .bg {
+    width: 60px;
+    height: 48px;
+    position: absolute;
+  }
+  .bg.bg1 {
+    top: -12px;
+    left: 24px;
+  }
+  .bg.bg2 {
+    bottom: -12px;
+    right: 24px;
+  }
+  .famous-word {
+    font-size: 16px;
+    color: #1c1f21;
+    text-align: left;
+    line-height: 24px;
+  }
+  .famous-word .author {
+    text-align: right;
+  }
+`;
